6634444219100978a263c8a104e7c82341f0a350
mark l chaves <mark@marklchaves.com>
2020-10-13 09:35:41 +0800

    More error handling

--- public/js/link-helper-divi.js
+++ public/js/link-helper-divi.js
@@ -1,16 +1,22 @@
 /** Add title attributes to image links if needed. */
 function addDiviLinkTitles() {
   let elts = document.querySelectorAll(php_vars.querySelector);
+  if (elts === undefined || elts === "") return;
   let eltsArr = Array.from(elts);
   eltsArr.map((elt) => {
-    let parent = elt.parentElement;
-    let linkTitle = parent.getAttribute("title");
-    if (linkTitle === undefined || linkTitle === "") {
-      let child = elt.firstElementChild;
-      let imgTitle = child.getAttribute("title");
-      let imgAlt = child.getAttribute("alt");
-      let imgTitleOrAlt = imgTitle ? imgTitle : imgAlt;
-      if ((imgTitleOrAlt !== undefined) && (imgTitleOrAlt !== "")) parent.setAttribute("title", imgTitleOrAlt);
+    try {
+      let parent = elt.parentElement;
+      let linkTitle = parent.getAttribute("title");
+      if (linkTitle === undefined || linkTitle === "") {
+        let child = elt.firstElementChild;
+        let imgTitle = child.getAttribute("title");
+        let imgAlt = child.getAttribute("alt");
+        let imgTitleOrAlt = imgTitle ? imgTitle : imgAlt;
+        if (imgTitleOrAlt !== undefined && imgTitleOrAlt !== "")
+          parent.setAttribute("title", imgTitleOrAlt);
+      }
+    } catch (e) {
+      console.log('Link Helper Div did not process ' + php_vars.querySelector);
     }
   });
 }
